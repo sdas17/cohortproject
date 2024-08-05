5e912217089132a5c592cf8e026651b28f257a1d
sdas1 <sdas1@miraclesoft.com>
2024-08-06 00:36:37 +0530

    assigment

--- session01/index.js
+++ session01/index.js
@@ -14,4 +14,17 @@ console.log(FullName)
 //const is can'be change they give stricteness
 
 //complexxtype 01:23
+// array in js=> array/vec c++ =>vectore in rust=>array list in java
+//object in js =>Map in c++=>Hashmap in rust=>hasmap in java
 
+let user = {
+	fullname: "Harkirat",
+	age: 19,
+    gender:"male"
+}
+function greet(user){
+console.log(`Hi Mr ${user.fullname}, your age is ${user.age}`);
+
+
+}
+greet(user);
